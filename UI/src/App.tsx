e608c7c0a4b2993542f1930212b3f9330d69aeac
marmoure <tinky93winky@gmail.com>
2023-12-22 02:45:24 +0100

    migrate to vite

---
+++ UI/src/App.tsx
@@ -0,0 +1,10 @@
+import Container from '@mui/material/Container';
+import Dashboard from './dashboard';
+
+export default function App() {
+  return (
+    <Container>
+      <Dashboard />
+    </Container>
+  );
+}
